30f68462f34d784f50cda2617f651e8a1f8bae17
Philip Domician <Philipnjau98@gmail.com>
2024-01-27 12:10:01 +0300

    Update helloRouter.js

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.get("/", async (req, res) => {
-  res.status(200).send({ message: "Hello Philip" });
+routes.get("/:name", async (req, res) => {
+  const { name } = req.params;
+  res.status(200).send({ message: `Hello ${name}` });
 });
 
 export default routes;
