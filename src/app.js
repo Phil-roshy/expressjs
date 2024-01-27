d57c4d14a9c88ace21910bbc0fe67dcdca39e318
Philip Domician <Philipnjau98@gmail.com>
2024-01-27 12:37:27 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,24 @@
+// app.js
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-
 import * as middleware from "./utils/middleware.js";
 import helloRoute from "./routes/helloRouter.js";
+import mathRoute from "./routes/mathRouter.js"; // Import the new router
 
 const app = express();
 
-// parse json request body
 app.use(express.json());
-
-// enable cors
 app.use(cors());
-
-// request logger middleware
 app.use(morgan("tiny"));
 
-// healthcheck endpoint
 app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
 app.use("/hello", helloRoute);
-app.use("/hello", mathRoute);
+app.use("/math", mathRoute); // Use the new router for math operations
 
-
-// custom middleware
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
